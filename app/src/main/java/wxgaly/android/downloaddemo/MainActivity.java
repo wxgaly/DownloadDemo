4959aa93903018819c6822fdfbc6e7786bfc8156
wangxuguang <nova_wangxuguang@126.com>
2018-06-14 17:11:48 +0800

    上传字体部分修改

--- app/src/main/java/wxgaly/android/downloaddemo/MainActivity.java
+++ app/src/main/java/wxgaly/android/downloaddemo/MainActivity.java
@@ -1,19 +1,30 @@
 package wxgaly.android.downloaddemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 import nova.android.wxgaly.downloaddemo.R;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import wxgaly.android.downloaddemo.api.FileInfoGetter;
 import wxgaly.android.downloaddemo.bean.FileInfo;
 import wxgaly.android.downloaddemo.bean.IDownloadCallback;
 import wxgaly.android.downloaddemo.util.DownloadUtils;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,12 +37,141 @@ public class MainActivity extends AppCompatActivity {
     private static final int NOTIFY_VALUE = 1024 * 1024 * 2;//1024 * 1024 *
     private List<Long> downloadList = new ArrayList<>(THREAD_COUNT);
 
+    public static final String CUSTOM_FONT_XINWEI = "/sdcard/nova/viplex_terminal/font/STXINWEI.TTF";
+    public static final String CUSTOM_FONT_XIHEI = "/sdcard/nova/viplex_terminal/font/STXIHEI.TTF";
+    public static final String CUSTOM_FONT_SIMLI = "/sdcard/nova/viplex_terminal/font/SIMLI.TTF";
+    public static final String CUSTOM_FONT_STSONG = "/sdcard/nova/viplex_terminal/font/STSONG.TTF";
+    public static final String CUSTOM_FONT_STKAITI = "/sdcard/nova/viplex_terminal/font/STKAITI.TTF";
+    public static final String CUSTOM_FONT_STLITI = "/sdcard/nova/viplex_terminal/font/STLITI.TTF";
+
+    public static final String CUSTOM_FONT_MICROSOFT_YAHEI = "Microsoft YaHei";
+    public static final String CUSTOM_FONT_SONGTI = "SimSun";
+    public static final String CUSTOM_FONT_KAITI = "KaiTi";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 //        testDownload();
+//        testFontHeight();
+        testShell();
+
+    }
+
+    private void testShell() {
+        enterShell();
+    }
+
+    public static boolean enterShell() {
+
+        boolean result = false;
+
+        try {
+
+            Process p = Runtime.getRuntime().exec("su");
+//            Process p = Runtime.getRuntime().exec("sh");
+
+
+            InputStream input = p.getInputStream();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(input));
+
+            StringBuffer stringBuffer = new StringBuffer();
+
+            String content = "";
+
+            // 读取ping的内容，可不加。
+            DataOutputStream  os = new DataOutputStream(p.getOutputStream());
+
+            os.write("echo AT+CRESET > /dev/ttyUSB2".getBytes());
+            os.writeBytes("\n");
+            os.flush();
+
+//            os.write("mkdir a".getBytes());
+//            os.writeBytes("\n");
+//            os.flush();
+
+
+            // PING的状态
+
+//            int status = p.waitFor();
+//
+//            if (status == 0) {
+//
+//                result = true;
+//
+//            }
+
+            Log.d(TAG, "enterShell: " + stringBuffer.toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public String execute(String[] command, String directory) throws IOException {
+        String result = "";
+        String[] suC = {"su"};
+        try {
+
+            ProcessBuilder builder = new ProcessBuilder(suC);
+            if (directory != null)
+                builder.directory(new File(directory));
+            builder.redirectErrorStream(true);
+            Process process = builder.start();
+            DataOutputStream os;
+            os = new DataOutputStream(process.getOutputStream());
+            InputStream is = process.getInputStream();
+
+            os.writeBytes("ls\n");
+            os.flush();
+            os.close();
+            byte[] buffer = new byte[1024];
+            while (is.read(buffer) != -1) {
+                result = result + new String(buffer);
+            }
+            is.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private void testFontHeight() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int textSize = 10;
+
+                while (textSize <= 100) {
+                    initPaint(textSize);
+                    textSize += 10;
+                }
+            }
+        }).start();
+    }
+
+    private void initPaint(int textSize) {
+        Paint paint = new Paint();
+        try {
+            Typeface typeface = Typeface.createFromFile(CUSTOM_FONT_XIHEI);
+//            Typeface typeface = Typeface.create(CUSTOM_FONT_KAITI, Typeface.NORMAL);
+
+            paint.setTypeface(typeface);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        paint.setColor(Color.RED);
+        paint.setTextSize(textSize);
+        Rect rect = new Rect();
+        paint.getTextBounds("真", 0, 1, rect);
+        Paint.FontMetrics fontMetrics = paint.getFontMetrics();
+//        Log.d(TAG, "textSize : " + textSize + "----height: " + (fontMetrics.bottom - fontMetrics.top));
+//        Log.d(TAG, " rect ----height: " + rect.height());
     }
 
     private void testDownload() {
--- app/src/main/java/wxgaly/android/downloaddemo/view/CustomTextView.java
+++ app/src/main/java/wxgaly/android/downloaddemo/view/CustomTextView.java
@@ -7,6 +7,7 @@ import android.graphics.LinearGradient;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.RadialGradient;
+import android.graphics.Rect;
 import android.graphics.Shader;
 import android.graphics.SweepGradient;
 import android.graphics.Typeface;
@@ -26,18 +27,17 @@ import java.util.regex.Pattern;
 
 public class CustomTextView extends android.support.v7.widget.AppCompatTextView {
 
-    public static final String CUSTOM_FONT_XINWEI = "/sdcard/nova/viplex_terminal/font/STXINWEI" +
-            ".TTF";
+    public static final String CUSTOM_FONT_XINWEI = "/sdcard/nova/viplex_terminal/font/STXINWEI.TTF";
     public static final String CUSTOM_FONT_XIHEI = "/sdcard/nova/viplex_terminal/font/STXIHEI.TTF";
     public static final String CUSTOM_FONT_KRUTI = "/sdcard/nova/viplex_terminal/font/Kruti_Dev.ttf";
     private static final String TAG = "wxg";
     private static final String TEXT = "请输入文字";
-    private static final int TEXT_SIZE = 60;
+    private static final int TEXT_SIZE = 90;
     private static final String TEXT_INDIA = "请输入ஆனால் நான் hello world உன்னை புரிந்து கொள்ள முடியவில்லை దయచేసి " +
             "టెక్స్ట్ని నమోదు చేయండి知道吗 कृपया पाठ दर्ज करे ದಯವಿಟ್ಟು ಪಠ್ಯವನ್ನು ನಮೂದಿಸಿ";
     private static final String TEXT_ENGLISH = "This is my treasure, health is the first wealth in life.";
     private static final String TEXT_YINDI = "कृपया पाठ दर्ज करेंऐसा करने के लिए कई चीजें होंगी.";
-    private static final char[] chars = TEXT.toCharArray();
+    //    private static final char[] chars = TEXT.toCharArray();
     private static final int[] colors = new int[]{Color.RED, Color.BLUE, Color.GREEN, Color.GRAY,
             Color.CYAN, Color
             .BLACK, Color.WHITE, Color.YELLOW};
@@ -87,7 +87,7 @@ public class CustomTextView extends android.support.v7.widget.AppCompatTextView 
     private void initPaint() {
         Paint paint = mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         try {
-            Typeface typeface = Typeface.createFromFile(CUSTOM_FONT_KRUTI);
+            Typeface typeface = Typeface.createFromFile(CUSTOM_FONT_XIHEI);
             paint.setTypeface(typeface);
         } catch (Exception e) {
             e.printStackTrace();
@@ -127,13 +127,16 @@ public class CustomTextView extends android.support.v7.widget.AppCompatTextView 
         Paint.FontMetrics fontMetrics = mPaint.getFontMetrics();
 //        Log.d(TAG, "ascent: " + fontMetrics.ascent);
 //        Log.d(TAG, "descent: " + fontMetrics.descent);
+//        Log.d(TAG, "bottom: " + fontMetrics.bottom);
+//        Log.d(TAG, "top: " + fontMetrics.top);
         int baseLine = (int) ((getHeight() - fontMetrics.bottom - fontMetrics.top) / 2);
 
 //        canvas.drawRect(new Rect(0, 0, (int) measureText, (int) (fontMetrics.bottom -
 // fontMetrics.top)), mPaint);
 //        mPaint.setColor(Color.RED);
 
-        drawIndiaText(canvas);
+        drawLineText(canvas, "my life");
+//        drawIndiaText(canvas, TEXT);
 
 //        canvas.drawText(TEXT, x, baseLine, mPaint);
 //        x -= 2;
@@ -153,25 +156,80 @@ public class CustomTextView extends android.support.v7.widget.AppCompatTextView 
 
     }
 
-    private void drawIndiaText(Canvas canvas) {
-        float textWidth = mPaint.measureText(TEXT_YINDI);
+    private void drawLineText(Canvas canvas, String text) {
+
+        int realWidth = 0;
+        float textWidth = 0;
+        char[] chars = text.toCharArray();
+        int lineCount = 0;
+
+        Rect rect = new Rect();
+        mPaint.getTextBounds("y", 0, 1, rect);
+        Paint.FontMetrics fontMetrics = mPaint.getFontMetrics();
+        float descent = fontMetrics.descent;
+
+        float baseLine = (getHeight() - fontMetrics.bottom - fontMetrics.top) / 2;
+        float ascent = fontMetrics.ascent;
+        Log.d(TAG, "drawLineText: " + baseLine);
+        Log.d(TAG, "ascent: " + ascent);
+        Log.d(TAG, "descent: " + descent);
+        Log.d(TAG, "rect.height(): " + rect.height());
+
+        Paint paintBaseLine = new Paint();
+        paintBaseLine.setColor(Color.GREEN);
+        paintBaseLine.setStrokeWidth(1);
+
+        Paint paintAscent = new Paint();
+        paintAscent.setColor(Color.WHITE);
+        paintAscent.setStrokeWidth(1);
+
+        Paint paintDescent = new Paint();
+        paintDescent.setColor(Color.BLUE);
+        paintDescent.setStrokeWidth(1);
+
+        for (int i = 0; i < chars.length; i++) {
+            if (realWidth > getMeasuredWidth()) {
+                realWidth = 0;
+                lineCount += 1;
+
+            }
+            canvas.drawText(chars, i, 1, realWidth, baseLine * (lineCount + 1), mPaint);
+
+            canvas.drawLine(realWidth, baseLine * (lineCount + 1), realWidth + mPaint.measureText(chars, i, 1),
+                    baseLine * (lineCount + 1), paintBaseLine);
+
+            canvas.drawLine(realWidth, (baseLine + ascent) * (lineCount + 1), realWidth + mPaint.measureText(chars,
+                    i, 1), (baseLine + ascent) * (lineCount + 1), paintAscent);
+
+            canvas.drawLine(realWidth, (baseLine + descent) * (lineCount + 1), realWidth + mPaint.measureText(chars, i,
+                    1), (baseLine + descent) * (lineCount + 1), paintDescent);
+
+            textWidth = mPaint.measureText(chars, i, 1);
+            realWidth += textWidth;
+        }
+
+    }
+
+    private void drawIndiaText(Canvas canvas, String text) {
+        float textWidth = mPaint.measureText(text);
         int start = 0;
         int realCount = 1;
         float realWidth = 0f;
         int lineCount = 0;
-        int len = TEXT_YINDI.length();
+        int len = text.length();
         Paint.FontMetrics fontMetrics = mPaint.getFontMetrics();
         Log.d(TAG, "drawIndiaText: " + (fontMetrics.bottom - fontMetrics.top));
 //        int baseLine = (int) ((getHeight() - fontMetrics.bottom - fontMetrics.top) / 2);
-        float baseLine = (fontMetrics.bottom - fontMetrics.top - fontMetrics.bottom - fontMetrics.top) / 2;
+        float baseLine = fontMetrics.bottom - fontMetrics.top;
+
         Log.d(TAG, "baseLine: " + baseLine);
 
         while ((start + realCount) <= len) {
-            realWidth = mPaint.measureText(TEXT_YINDI, start, start + realCount);
+            realWidth = mPaint.measureText(text, start, start + realCount);
 
             if (realWidth > getMeasuredWidth()) {
 
-                String str = TEXT_YINDI.substring(start + realCount - 1, start + realCount);
+                String str = text.substring(start + realCount - 1, start + realCount);
                 int tempRealCount = realCount;
 
                 while (!str.contains(SPACE_STR) && (checkIsContainsIndia(str) || checkIsContainsEnglish(str))) {
@@ -183,16 +241,16 @@ public class CustomTextView extends android.support.v7.widget.AppCompatTextView 
                             if (start + realCount >= len) {
                                 break;
                             }
-                            str = TEXT_YINDI.substring(start + realCount, start + realCount + 1);
+                            str = text.substring(start + realCount, start + realCount + 1);
                             realCount++;
                         }
                         break;
                     }
 
-                    str = TEXT_YINDI.substring(start + realCount - 1, start + realCount);
+                    str = text.substring(start + realCount - 1, start + realCount);
                 }
 
-                String drawText = TEXT_YINDI.substring(start, start + realCount);
+                String drawText = text.substring(start, start + realCount);
 //                if (!TextUtils.isEmpty(drawText.trim())) {
                 canvas.drawText(drawText, 0, baseLine + TEXT_SIZE * lineCount, mPaint);
 //                Log.d(TAG, "drawIndiaText: " + TEXT_INDIA.substring(start, start + realCount) + "---realWidth : " +
