fefb565827d764f062e5010ed51e3f924cfc1925
wangxuguang <nova_wangxuguang@126.com>
2018-07-31 19:29:52 +0800

    1. 修改适配器

--- slidingcarddemo/src/main/java/nova/android/slidingcarddemo/adapter/RecyclerAdapter.kt
+++ slidingcarddemo/src/main/java/nova/android/slidingcarddemo/adapter/RecyclerAdapter.kt
@@ -13,7 +13,7 @@ import nova.android.slidingcarddemo.R
  * @author Created by WXG on 2018/7/31 031 19:07.
  * @version V1.0
  */
-class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.RecyclerHolder>() {
+class RecyclerAdapter(recyclerView: RecyclerView) : RecyclerView.Adapter<RecyclerAdapter.RecyclerHolder>() {
 
     private val ITEMS = arrayListOf(
             "赵丽颖", "Angelababy", "林心如",
@@ -22,9 +22,30 @@ class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.RecyclerHolder>() {
             "唐嫣", "刘亦菲", "刘诗诗",
             "高圆圆", "李沁", "郑爽")
 
+    private var inflater: LayoutInflater? = null
+
+    init {
+        inflater = LayoutInflater.from(recyclerView.context)
+
+//        recyclerView.addItemDecoration(object : RecyclerView.ItemDecoration() {
+//
+//            override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State?) {
+//                super.getItemOffsets(outRect, view, parent, state)
+//                val position = parent.getChildViewHolder(view).adapterPosition
+//                val offset = 10
+//
+//                outRect.set(offset, if (position == 0) {
+//                    offset
+//                } else {
+//                    0
+//                }, offset, offset)
+//            }
+//        })
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerHolder {
 
-        return RecyclerHolder(LayoutInflater.from(parent.context).inflate(R.layout.card_item, parent, false))
+        return RecyclerHolder(inflater?.inflate(R.layout.card_item, parent, false))
     }
 
     override fun getItemCount(): Int = ITEMS.size
@@ -33,12 +54,12 @@ class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.RecyclerHolder>() {
 
     }
 
-    inner class RecyclerHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class RecyclerHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
 
-        var tv: TextView = itemView.findViewById(R.id.tv_item)
+        var tv: TextView? = itemView?.findViewById(R.id.tv_item)
 
         init {
-            itemView.isClickable = true
+            itemView?.isClickable = true
         }
     }
 
